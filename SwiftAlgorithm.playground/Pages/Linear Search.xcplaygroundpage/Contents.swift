6b821872e40b370e12c26ace24f667d591699cd3
Sangmin Kim <offiksm0903@naver.com>
2025-03-29 17:14:51 +0900

    Add Linear Search

---
+++ SwiftAlgorithm.playground/Pages/Linear Search.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+func linearSearch<T: Equatable>(_ data: T, in arr: [T]) -> Bool {
+    for element in arr {
+        if element == data {
+            return true
+        }
+    }
+    
+    return false
+}
+
+let arr = ["q", "w", "e", "r", "a", "s", "d", "f"]
+
+print(linearSearch("a", in: arr))
+print(linearSearch("i", in: arr))
