1ec0fa740010ccbbcdbec1e46ac7a24753db2ddd
Sangmin Kim <offiksm0903@naver.com>
2025-04-04 16:08:41 +0900

    Add Selection Sort

---
+++ SwiftAlgorithm.playground/Pages/Selection Sort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func selectionSort(_ arr: [Int]) -> [Int] {
+    var sorted = arr
+    
+    for i in 0..<sorted.count-1 {
+        for j in i+1..<sorted.count {
+            if sorted[j] < sorted[i] {
+                sorted.swapAt(i, j)
+            }
+        }
+    }
+    
+    return sorted
+}
+
+let arr = [10, -3, 0, 8, 7, 2]
+let sorted = selectionSort(arr)
+print(sorted)
+
+//: [Next](@next)
